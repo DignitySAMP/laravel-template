79131eaf28494c667ed42653f074251e559432f4
DignitySAMP <mionedur4@gmail.com>
2025-12-31 14:12:53 +0100

    refactor(forgot-password): implement new components

--- resources/js/pages/auth/ForgotPassword.vue
+++ resources/js/pages/auth/ForgotPassword.vue
@@ -1,16 +1,14 @@
 <script setup lang="ts">
-import InputError from '@/components/InputError.vue';
-import TextLink from '@/components/TextLink.vue';
-import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
-import { Spinner } from '@/components/ui/spinner';
+
+import AppButton from '@/components/AppButton.vue';
+import AppInput from '@/components/AppInput.vue';
+import AppLink from '@/components/AppLink.vue';
 import AuthLayout from '@/layouts/AuthLayout.vue';
 import { login } from '@/routes';
 import { email } from '@/routes/password';
 import { Form, Head } from '@inertiajs/vue3';
 
-defineProps<{
+const props = defineProps<{
     status?: string;
 }>();
 </script>
@@ -23,42 +21,41 @@ defineProps<{
         <Head title="Forgot password" />
 
         <div
-            v-if="status"
+            v-if="props.status"
             class="mb-4 text-center text-sm font-medium text-green-600"
         >
-            {{ status }}
+            {{ props.status }}
         </div>
 
         <div class="space-y-6">
             <Form v-bind="email.form()" v-slot="{ errors, processing }">
                 <div class="grid gap-2">
-                    <Label for="email">Email address</Label>
-                    <Input
-                        id="email"
-                        type="email"
+                    <AppInput
                         name="email"
-                        autocomplete="off"
-                        autofocus
+                        label="Email address"
+                        type="email"
                         placeholder="email@example.com"
+                        :autofocus="true"
+                        autocomplete="off"
+                        :error="errors.email"
+                        :required="true"
                     />
-                    <InputError :message="errors.email" />
                 </div>
 
                 <div class="my-6 flex items-center justify-start">
-                    <Button
-                        class="w-full"
+
+                    <AppButton
+                        name="request-new-password"
+                        type="submit"
                         :disabled="processing"
-                        data-test="email-password-reset-link-button"
-                    >
-                        <Spinner v-if="processing" />
-                        Email password reset link
-                    </Button>
+                        text="Email password reset link"
+                    />
                 </div>
             </Form>
 
             <div class="space-x-1 text-center text-sm text-muted-foreground">
                 <span>Or, return to</span>
-                <TextLink :href="login()">log in</TextLink>
+                <AppLink :href="login()">log in</AppLink>
             </div>
         </div>
     </AuthLayout>
