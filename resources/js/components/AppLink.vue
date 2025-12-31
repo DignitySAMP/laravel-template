79b5625d75053841bc2d0cbf0963163449a243c9
DignitySAMP <mionedur4@gmail.com>
2025-12-31 10:45:13 +0100

    ✨ feat: implement wip ui components

---
+++ resources/js/components/AppLink.vue
@@ -0,0 +1,25 @@
+<template>
+    <Link
+        :href="props.href"
+        :tabindex="props.tabindex"
+        :method="props.method"
+        :as="props.as"
+        class="text-foreground underline decoration-neutral-300 underline-offset-4 transition-colors duration-300 ease-out hover:decoration-current! dark:decoration-neutral-500"
+    >
+        <slot />
+    </Link>
+</template>
+<script setup lang="ts">
+
+    import { LinkComponentBaseProps, Method } from '@inertiajs/core';
+    import { Link } from '@inertiajs/vue3';
+
+    interface Props {
+        href: LinkComponentBaseProps['href'];
+        tabindex?: number;
+        method?: Method;
+        as?: string;
+    }
+
+    const props = defineProps<Props>();
+</script>
