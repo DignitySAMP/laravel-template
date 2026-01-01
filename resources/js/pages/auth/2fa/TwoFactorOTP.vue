5493d3de9615768b0fbb83ed8a36b3295564f269
DignitySAMP <mionedur4@gmail.com>
2026-01-01 01:50:19 +0100

    refactor(2fa): add new components to 2fa page, partialize page into 2 files + add useForm instead of Form

---
+++ resources/js/pages/auth/2fa/TwoFactorOTP.vue
@@ -0,0 +1,38 @@
+<template>
+    <form @submit.prevent>
+        <div
+            class="flex flex-col items-center justify-center space-y-3 text-center"
+        >
+            <div class="flex w-full items-center justify-center">
+                <AppInputOTP id="otp" v-model="form.code" name="otp" :error="form.errors.code"/>
+            </div>
+        </div>
+        <AppButton 
+            @click="submit" 
+            text="Continue" 
+            name="btn_otp" 
+            type="submit" 
+            class="mt-4 w-full" 
+            :disabled="form.processing"
+        />
+    </form>
+
+</template>
+<script setup lang="ts">
+
+import AppButton from '@/components/AppButton.vue';
+import AppInputOTP from '@/components/AppInputOTP.vue';
+import { store } from '@/routes/two-factor/login';
+import { useForm } from '@inertiajs/vue3';
+
+const form = useForm({
+    code: ''
+})
+
+const submit = () => {
+    form.submit(store(), {
+        preserveScroll: true,
+        onFinish: () => form.reset('code'),
+    });
+}
+</script>
