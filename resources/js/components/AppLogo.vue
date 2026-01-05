5a99414c7f68e4ed27fb8d7aa9121535b039c018
Davy <davy@datalink.be>
2026-01-05 16:38:15 +0100

    ✨ feat: add size to AppLogo, implement in AuthLayout

--- resources/js/components/AppLogo.vue
+++ resources/js/components/AppLogo.vue
@@ -1,9 +1,17 @@
 <template>
-    <Link :href="home().url" class="text-xl font-semibold hover:scale-110 transition duration-300">
+    <Link :href="home().url" class="font-semibold hover:scale-110 transition duration-300" :class="[
+        props.size === 'large' ? 'text-4xl' : 'text-lg'
+    ]">
         {{ usePage().props.name }}
     </Link>
 </template>
 <script setup lang="ts">
     import { home } from '@/routes';
     import { Link, usePage } from '@inertiajs/vue3'
+
+    const props = withDefaults(defineProps<{
+        size?: 'small' | 'large'
+    }>(), {
+        size: 'small'
+    });
 </script>
--- resources/js/layouts/AuthLayout.vue
+++ resources/js/layouts/AuthLayout.vue
@@ -1,13 +1,18 @@
 <script setup lang="ts">
-defineProps<{
-	title?: string
-	description?: string
-}>()
+	import AppLogo from '@/components/AppLogo.vue';
+	defineProps<{
+		title?: string
+		description?: string
+	}>()
 </script>
 
 <template>
 	<div class="min-h-screen flex items-center justify-center px-4 py-12">
 		<div class="w-full max-w-md">
+			<div class="w-full flex justify-center items-center mb-4">
+				<AppLogo size="large"/>
+			</div>
+
 			<div class="text-center mb-8">
 				<h1 class="text-2xl font-bold">{{ title }}</h1>
 				<p class="text-gray-600 mt-2">{{ description }}</p>
