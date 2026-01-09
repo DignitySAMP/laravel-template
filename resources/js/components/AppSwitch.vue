8951b1bb758e067e076295903f4497c540e321ed
Davy <davy@datalink.be>
2026-01-09 14:48:49 +0100

    ✨ feat: add AppSwitch component for toggling true/false

---
+++ resources/js/components/AppSwitch.vue
@@ -0,0 +1,18 @@
+<template>
+	<div
+		@click="model = !model"
+		class="w-10 p-0.5 cursor-pointer font-medium bg-white rounded-xl shadow-sm relative group"
+	>
+		<div
+			class="rounded-full size-4 p-1 transition-transform duration-300 ease-in-out"
+			:class="{
+				'bg-black group-hover:bg-gray-700 translate-x-5': model,
+				'bg-gray-300 group-hover:bg-gray-400 translate-x-0': !model,
+			}"
+		/>
+	</div>
+</template>
+
+<script setup lang="ts">
+    const model = defineModel<boolean>()
+</script>
