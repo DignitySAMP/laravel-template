1c66dc7ae686843921525556e0ed86b1438ab5d1
Davy <davy@datalink.be>
2026-01-09 09:08:38 +0100

    🕰️ wip: cookie management composable and wip AppCookies component

---
+++ resources/js/components/AppCookies.vue
@@ -0,0 +1,34 @@
+<template>
+	<div
+		v-if="showCookieList"
+		class="bg-white rounded-xl shadow-sm"
+	>
+		<div class="flex flex-col gap-2">
+			<div
+				v-for="(cookie, index) in CookieContainer"
+				:key="index"
+				class="grid grid-cols-4 gap-2 bg-gray-50 border border-gray-100 rounded-lg"
+			>
+				<span>
+					{{ cookie.name }}
+				</span>
+
+				<span class="text-wrap">
+					{{ cookie.description }}
+				</span>
+
+				<span>
+					{{ getCookieTypeName(cookie.type)?.name ?? 'Unknown' }}
+				</span>
+
+				<span>{{ cookie.lifetime }} minutes ({{ cookie.lifetime / 60 }} hours)</span>
+			</div>
+		</div>
+	</div>
+</template>
+<script setup lang="ts">
+import { ref } from 'vue'
+import { CookieContainer, getCookieTypeName } from '@/composables/cookieHandler'
+const showCookieList = ref<boolean>(false)
+// TODO: Make the actual banner, make individual cookies toggable (make a new model for this so we can handle the choice on the backend)
+</script>
---
+++ resources/js/composables/cookieHandler.ts
@@ -0,0 +1,63 @@
+export enum CookieTypes {
+	COOKIE_TYPE_NECESSARY,
+	COOKIE_TYPE_FUNCTIONAL, // language preferences, dark mode, ...
+	COOKIE_TYPE_ANALYTICS, // analytics
+	COOKIE_TYPE_MARKETING, // browsing habits to show specific ads
+}
+
+export interface Cookie {
+	name: string
+	description: string
+	type: CookieTypes
+	lifetime: number // minutes
+}
+
+export const CookieContainer: Cookie[] = [
+	{
+		name: 'session',
+		description:
+			'Stores your current browser session. This allows us to persist your login state across sessions and HTTP requests',
+		type: CookieTypes.COOKIE_TYPE_NECESSARY,
+		lifetime: 120,
+	},
+	{
+		name: 'XSRF-TOKEN',
+		description:
+			'A token that is sent with requests you make to the application to prevent unauthorized requests (to make sure it is you who is making the request).',
+		type: CookieTypes.COOKIE_TYPE_NECESSARY,
+		lifetime: 120,
+	},
+]
+
+export function getCookieTypeName(type: CookieTypes) {
+	switch (type) {
+		case CookieTypes.COOKIE_TYPE_NECESSARY: {
+			return {
+				name: 'Necessary',
+				description:
+					'These cookies are first party and necessary for the website to function. Think about remembering your log in state across sessions.',
+			}
+		}
+		case CookieTypes.COOKIE_TYPE_FUNCTIONAL: {
+			return {
+				name: 'Functional',
+				description:
+					'These cookies are first party and provide function to the website, such as remembering your language preferences or website theme.',
+			}
+		}
+		case CookieTypes.COOKIE_TYPE_ANALYTICS: {
+			return {
+				name: 'Functional',
+				description:
+					'These cookies are third party and track how you use the website. We use this information to improve performance and make our website better.',
+			}
+		}
+		case CookieTypes.COOKIE_TYPE_MARKETING: {
+			return {
+				name: 'Functional',
+				description:
+					'These cookies are third party and use your browser preferences to show advertisements catered to your needs.',
+			}
+		}
+	}
+}
